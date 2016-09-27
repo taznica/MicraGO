be9f098848c7f9034c64ee110080cc0206165e52
ttsutchi <tailegog@gmail.com>
2016-09-27 23:33:17 +0900

    Make cube.json and register but not worked.

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,8 +1,10 @@
 package com.example.examplemod;
 
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -23,12 +25,19 @@ public class ExampleMod
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        GameRegistry.register(cube, new ResourceLocation(MODID, "cube"));
+
         int trackingRange = 80;
         int updateFrequency = 3;
         boolean sendVelocityUpdates = true;
+
+        GameRegistry.register(cube, new ResourceLocation(MODID, "cube"));
         EntityRegistry.registerModEntity(EntityCube.class, "entityCube", 0, this, trackingRange, updateFrequency, sendVelocityUpdates, 2243405, 7375001);
 //        EntityRegistry.addSpawn(Entity.class, 8, 4, 4, EnumCreatureType.AMBIENT, BiomeGenBase.getBiome(0));
+
+        if (event.getSide().isClient()) {
+            //1IDで複数モデルを登録するなら、上のメソッドで登録した登録名を指定する。
+            ModelLoader.setCustomModelResourceLocation(cube, 0, new ModelResourceLocation(MODID + ":" + "cube", "inventory"));
+        }
 
         GameRegistry.register(fullCube, new ResourceLocation(MODID, "fullCube"));
         EntityRegistry.registerModEntity(EntityFullCube.class, "entityFullCube", 0, this, trackingRange, updateFrequency, sendVelocityUpdates, 2243405, 7375001);
